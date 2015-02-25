210b7f188742958fa9d217b64768d9eeeb340a25
Omkar <puremella@gmail.com>
2015-02-25 22:12:05 +1100

    Boolean regex

---
+++ boolean_regex.rb
@@ -0,0 +1,15 @@
+#boolean_regex.rb
+
+def has_a_b?(string)
+
+if string =~ /b/
+puts "we have a match!"
+else
+	puts "No match here."
+end
+end
+
+has_a_b?("basketball")
+has_a_b?("football")
+has_a_b?('cricket')
+has_a_b?('baseball')
