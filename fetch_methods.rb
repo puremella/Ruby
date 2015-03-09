19f7cb9cf7df71f1bcc7abf69ec7fc4c7537ebfa
Omkar <puremella@gmail.com>
2015-03-09 14:05:57 +1100

    Ruby

---
+++ fetch_methods.rb
@@ -0,0 +1,9 @@
+# fetch_methods.rb
+
+person = {name: "bob", occupation: "web developer", hobbies: "painting"}
+
+if person.has_value?("bobi")
+	p "bob is in the hash"
+else
+	p "bob is not in the hash"
+end
