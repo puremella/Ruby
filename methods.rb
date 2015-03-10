e0649671e75db07d2559ff0a89a6e05bd59ccdfe
Omkar <puremella@gmail.com>
2015-03-10 14:30:40 +1100

    Ruby

---
+++ methods.rb
@@ -0,0 +1,15 @@
+def multiply(num1, num2)
+	num1 * num2
+end
+
+def add(num1, num2)
+	num1 + num2
+end
+
+def substarct(num1, num2)
+	num1 - num2
+end
+
+puts add(40,50)
+puts substarct(10, 20)
+puts multiply(add(40,50),substarct(10,20))
