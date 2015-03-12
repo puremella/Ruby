dca0201b20889078e379958dee7779d189e674f0
Omkar <puremella@gmail.com>
2015-03-12 12:27:02 +1100

    modified

--- input_from_user.rb
+++ input_from_user.rb
@@ -1,18 +1,20 @@
 # input_from_user.rb
 
-# x = gets.chomp.to_s
-
-# begin
-# 	puts x
-# 	x = gets.chomp.to_s
-	
-# end while x != 'STOP'
 
 
-x = ""
-while x != "STOP" do
-  puts "Hi, How are you feeling?"
-  ans = gets.chomp
-  puts "Want me to ask you again?"
-  x = gets.chomp
-end
+begin userinput = gets.chomp.to_s
+
+	if userinput != 'STOP'
+		puts "You typed: " + userinput + " type STOP to exit"
+	end
+
+end while userinput.swapcase != "STOP"
+
+
+# x = ""
+# while x != "STOP" do
+#   puts "Hi, How are you feeling?"
+#   ans = gets.chomp
+#   puts "Want me to ask you again?"
+#   x = gets.chomp
+# end
