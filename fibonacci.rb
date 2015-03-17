b2552a9fb50f15758400a88885d9b178da973a91
Omkar <puremella@gmail.com>
2015-03-17 22:28:09 +1100

    Update fibonacci.rb

--- fibonacci.rb
+++ fibonacci.rb
@@ -12,9 +12,10 @@ end
 
 puts fibonacci(6)
 
-#factorial of numbers 5..8
+#factorial of numbers 5..9
 puts 5*4*3*2*1
 puts 6*5*4*3*2*1
 puts 7*6*5*4*3*2*1
 puts 8*7*6*5*4*3*2*1
+puts 9*8*7*6*5*4*3*2*1
 
